864507da675f553c341b9964c8eb1ff654034efb
GaneshTaddi-Devops <taddiganesh666@gmail.com>
2025-04-16 16:26:13 +0530

    Update main.tf
    added backend details to local state main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -22,3 +22,17 @@ resource "aws_instance" "app_server" {
   }
 }
 
+#-----------------------------------------------------
+# Added backend details         
+#-----------------------------------------------------
+terraform {
+  backend "s3" {
+    bucket         = "183631335733-terraform-states"
+    key            = "some_environment/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    kms_key_id     = "THE_ID_OF_THE_KMS_KEY"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
